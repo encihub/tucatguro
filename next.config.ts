0d2c11c43f3ceccf79bd4ebe5a0f2914adba869f
Javier López Úbeda <jlopezcur@gmail.com>
2025-11-03 01:13:21 +0100

    ci: remove temporal prefix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import { NextConfig } from 'next';
 
-const isCI = process.env.GITHUB_ACTIONS === 'true';
-const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
-const basePath = isCI ? `/${repo}` : '';
-
 export default {
   output: 'export',
   reactStrictMode: true,
   images: { unoptimized: true },
   trailingSlash: true,
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : '',
 } satisfies NextConfig;
