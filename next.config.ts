f61252c7152ce111b4c1e780dd47f19e3449efe8
Javier López Úbeda <jlopezcur@gmail.com>
2025-11-03 01:01:46 +0100

    ci: test temporal solution

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import { NextConfig } from 'next';
 
+const isCI = process.env.GITHUB_ACTIONS === 'true';
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const basePath = isCI ? `/${repo}` : '';
+
 export default {
   output: 'export',
   reactStrictMode: true,
   images: { unoptimized: true },
   trailingSlash: true,
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : '',
 } satisfies NextConfig;
